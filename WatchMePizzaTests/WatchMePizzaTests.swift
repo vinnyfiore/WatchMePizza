a3b407e09fa0ed3f9cc9267012be3ad9f5a2c5a1
vinnyfiore <vinnydakid@gmail.com>
2015-02-24 15:57:47 -0500

    Initial Commit

---
+++ WatchMePizzaTests/WatchMePizzaTests.swift
@@ -0,0 +1,36 @@
+//
+//  WatchMePizzaTests.swift
+//  WatchMePizzaTests
+//
+//  Created by Vincent Fiore on 2/24/15.
+//  Copyright (c) 2015 Vinny Fiore Apps. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class WatchMePizzaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
